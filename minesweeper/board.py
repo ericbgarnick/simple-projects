103d4c8f2ecbe0c62eb3f28a4c39c94aceeeedf6
Eric Garnick <eric.b.garnick@gmail.com>
2019-11-16 21:53:17 -0800

    Board repr almost works - needs UT and debug

--- minesweeper/board.py
+++ minesweeper/board.py
@@ -1,7 +1,7 @@
 import random
 import sys
 from enum import Enum
-from typing import Set, List, Union
+from typing import Set, List
 
 
 class GameDifficulty(Enum):
@@ -19,10 +19,11 @@ class Board:
     def __init__(self, size: int, difficulty: GameDifficulty):
         self._board_size = size
         self._bomb_locs = self._create_bombs(self.PCT_BOMBS[difficulty])
+        self._guesses = {}  # guessed index -> value to show
 
     def _create_bombs(self, pct_bombs: int) -> Set[int]:
         """
-        Fill squares in self._board with as many bombs
+        Return a set of indices fpr as many bombs
         as indicated by pct_bombs (% total spaces).
         """
         num_spaces = self._board_size ** 2
@@ -35,6 +36,52 @@ class Board:
             free_spaces.remove(bomb_space)
         return bomb_idxs
 
+    def _build_board_repr(self) -> List[List[str]]:
+        board_repr = []
+        for i in range(self._board_size ** 2):
+            if i in self._bomb_locs:
+                symbol = "*"
+            else:
+                symbol = self._guesses.get(i) or self._calc_symbol(i)
+            if not i % self._board_size:
+                board_repr.append([symbol])
+            else:
+                board_repr[-1].append(symbol)
+        return board_repr
+
+    def _calc_symbol(self, i: int) -> str:
+        neighbors = self._get_neighbors(i)
+        num_bomb_neighbors = len(neighbors & self._bomb_locs)
+        return str(num_bomb_neighbors) if num_bomb_neighbors else '_'
+
+    def _get_neighbors(self, i: int) -> Set[int]:
+        neighbors = set()
+        north = south = east = west = -1
+
+        if i >= self._board_size:
+            north = i - self._board_size
+        if i < self._board_size * (self._board_size - 1):
+            south = i + self._board_size
+        if i % self._board_size:
+            west = i - 1
+        if i % self._board_size != self._board_size - 1:
+            east = i + 1
+
+        if north != -1:
+            neighbors.add(north)
+            if east != -1:
+                neighbors.add(north + 1)
+            if west != -1:
+                neighbors.add(north - 1)
+        if south != -1:
+            neighbors.add(south)
+            if east != -1:
+                neighbors.add(south + 1)
+            if west != -1:
+                neighbors.add(south - 1)
+
+        return neighbors
+
     @property
     def board_size(self) -> int:
         return self._board_size
@@ -43,8 +90,12 @@ class Board:
     def bomb_locations(self) -> Set[int]:
         return self._bomb_locs
 
+    def __str__(self):
+        board_repr = self._build_board_repr()
+        return '\n'.join([' '.join(row) for row in board_repr])
+
 
 if __name__ == '__main__':
-    bsize = int(sys.argv[1])
-    b = Board(bsize, GameDifficulty.EASY)
+    board_size, difficulty = sys.argv[1:3]
+    b = Board(int(board_size), GameDifficulty[difficulty.upper()])
     print(b)
